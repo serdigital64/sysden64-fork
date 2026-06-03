b4adc0751f19991776a9ec6bd7b54c16cc6a567a
SerDigital64 (Max) <serdigital64@gmail.com>
2026-06-03 10:19:02 +0200

    real: promote 4.14.0 (#85)

--- src/lib/antigravityide.bash
+++ src/lib/antigravityide.bash
@@ -1,20 +1,20 @@
 # template: lib-config-2.0.0
-function module_antigravity_setup() {
+function module_antigravityide_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
   local module_type="$SYSDEN64_MODULE_TYPE_SHARED"
-  local module_name='antigravity'
+  local module_name='antigravityide'
   local module_etc=''
   local extra_locations=''
 
   [[ "$BL64_OS_TYPE" != "$BL64_OS_TYPE_LINUX" ]] && return 0
-  module_detect "$module_name" 'antigravity' 'Antigravity - AI IDE' "$extra_locations" || return 0
+  module_detect "$module_name" 'antigravity-ide' 'Antigravity IDE - AI IDE' "$extra_locations" || return 0
 
   module_etc="$(module_config_get_source "$module_type" "$module_name")" &&
-    module_antigravity_setup_config "$home" "$module_etc" "$module_name" "$module_type"
+    module_antigravityide_setup_config "$home" "$module_etc" "$module_name" "$module_type"
 }
 
-function module_antigravity_setup_config() {
+function module_antigravityide_setup_config() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
   local module_etc="$2"
@@ -26,7 +26,7 @@ function module_antigravity_setup_config() {
   local config_backup=''
 
   if [[ "$BL64_OS_TYPE" == "$BL64_OS_TYPE_LINUX" ]]; then
-    config_destination="${home}/.config/Antigravity"
+    config_destination="${home}/.config/Antigravity IDE"
     bl64_fs_dir_create \
       "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" \
       "${config_destination}" \
